57a79c5abdf825095d68082dd1680e42fc3ca1f5
jerry <susan.idea@gmail.com>
2021-07-31 18:30:06 +0800

    添加檢查檔案

--- memcached/reup.sh
+++ memcached/reup.sh
@@ -13,6 +13,14 @@ echo "*** 重啟 ***"
 
 echo "*************"
 
+# 檢查鎖定檔案
+if [ -f "./deploy.lock" ]; then
+  echo "準備重啟\n"
+else
+  echo "尚未建置過"
+  exit 1
+fi
+
 # docker-compose up
 docker-compose up -d
 
